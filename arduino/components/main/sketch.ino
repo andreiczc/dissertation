bf76b157214cba5b4c902fcf38cebaa121f4dff9
Andrei Cazacu <andrei.r.cazacu@gmail.com>
2022-01-02 21:36:03 +0200

    major changes

--- arduino/components/main/sketch.ino
+++ arduino/components/main/sketch.ino
@@ -1,22 +1,27 @@
+#include <ArduinoJson.h>
 #include <EEPROM.h>
 #include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 #include <FS.h>
-#include <GDBStub.h>
-
-#include "tinycbor.h"
+#include <tinycbor.h>
 
 #include "logger.h"
 
 #define byte uint8_t
 
-static const int OBJECT_ID   = 3301;
-static const int RESOURCE_ID = 5100;
-static int       INSTANCE_ID = 0;
-static int       BUFFER_SIZE = 128;
-static uint8_t   buffer[128];
-static Logger    logger(Serial);
-static String    ssid = "";
-static String    pass = "";
+static const int      OBJECT_ID   = 3301;
+static const int      RESOURCE_ID = 5100;
+static int            INSTANCE_ID = 0;
+static int            BUFFER_SIZE = 128;
+static const int      EEPROM_SIZE = 128;
+static uint8_t        buffer[128];
+static Logger         logger(Serial);
+static String         ssid    = "";
+static String         pass    = "";
+static String         content = "";
+static int            status_code;
+static AsyncWebServer server(80);
 
 static size_t cbor_encode(uint8_t *const payload, int buffer_size)
 {
@@ -77,17 +82,6 @@ static void dump_hex(Stream &stream, const uint8_t *const buffer, size_t size)
   stream.println();
 }
 
-static wl_status_t timed_wifi_status()
-{
-  short ctr = 30;
-  while (WiFi.status() != WL_CONNECTED && ctr--)
-  {
-    delay(500);
-  }
-
-  return WiFi.status();
-}
-
 static void read_wifi_credentials()
 {
   int  idx        = 0;
@@ -137,11 +131,29 @@ static void empty_wifi_credentials()
   }
 }
 
-static void start_wifi()
+static void clear_eeprom(bool commit = false)
 {
-  read_wifi_credentials();
+  for (int i = 0; i < EEPROM_SIZE; ++i)
+  {
+    EEPROM.write(i, 0);
+  }
+
+  if (commit)
+  {
+    EEPROM.commit();
+  }
+}
+
+static void start_wifi(bool in_memory = false)
+{
+  /* if (!in_memory)
+  {
+    read_wifi_credentials();
+  }
+
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, pass);
-  if (timed_wifi_status() == WL_CONNECTED)
+  if (WiFi.waitForConnectResult() == WL_CONNECTED)
   {
     logger.info("WiFi connection established!");
     empty_wifi_credentials();
@@ -150,18 +162,88 @@ static void start_wifi()
 
   store_wifi_credentials("WiFi-2.4",
                          "180898Delia!"); // will be replaced by SoftAP method
-  ESP.restart();
+  start_wifi(true); */
+
+  WiFi.begin("WiFi-2.4", "180898Delia!");
+  if (WiFi.waitForConnectResult() == WL_CONNECTED)
+  {
+    logger.info("WiFi connection established!");
+
+    return;
+  }
+}
+
+static String create_network_list()
+{
+  int    no_networks = WiFi.scanNetworks();
+  String result      = "";
+
+  for (int i = 0; i < no_networks; ++i)
+  {
+    result += "<option class=\"list-group-item\">";
+    result += WiFi.SSID(i);
+    result += "</option>";
+  }
+
+  return result;
+}
+
+static String processor(const String &var)
+{
+  if (var == "NETWORKS")
+  {
+    return create_network_list();
+  }
+
+  return String();
+}
+
+static void create_web_server()
+{
+  server.on("/", HTTP_GET,
+            [](AsyncWebServerRequest *request) {
+              request->send(SPIFFS, "/index.html", String(), false, processor);
+            });
+
+  server.on(
+      "/setCredentials", HTTP_POST,
+      [](AsyncWebServerRequest *request)
+      {
+        if (!request->hasArg("plain"))
+        {
+          request->send(400, "text/plain", "Bad request");
+
+          return;
+        }
+
+        DynamicJsonDocument doc(1024);
+        deserializeJson(doc, request->arg("plain"));
+
+        logger.info(doc["ssid"]);
+        logger.info(doc["pass"]);
+
+        request->send(
+            200, "text/plain",
+            "WiFi settings have been saved. The sensor will now restart!");
+      });
+
+  server.begin();
+
+  logger.info("Web Server is up!");
 }
 
 void setup()
 {
   Serial.begin(115200);
-  gdbstub_init();
-  delay(30 * 1000);
 
-  EEPROM.begin(128);
+  if (!SPIFFS.begin())
+  {
+    logger.error("Couldn't mount filesystem");
+  }
+  // EEPROM.begin(EEPROM_SIZE);
 
   start_wifi();
+  create_web_server();
 }
 
 void loop()
