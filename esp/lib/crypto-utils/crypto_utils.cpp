6a0d47e7b26bc6da1bd2d8d979c39f23ac9ac48e
Andrei Cazacu <andrei.cazacu@oracle.com>
2022-04-16 17:08:13 +0300

    generation of ecdh params done

--- esp/lib/crypto-utils/crypto_utils.cpp
+++ esp/lib/crypto-utils/crypto_utils.cpp
@@ -1,11 +1,17 @@
 #include "crypto_utils.h"
 
-#include "mbedtls/aes.h"
+#include "aes/esp_aes.h"
+#include "esp_log.h"
 #include "mbedtls/base64.h"
+#include "mbedtls/ctr_drbg.h"
+#include "mbedtls/ecdh.h"
+#include "mbedtls/entropy.h"
 #include <cstring>
 
 namespace crypto
 {
+static auto *TAG = "CRYPTO";
+
 std::unique_ptr<uint8_t[]> encodeBase64(uint8_t *input)
 {
   const auto inputLength = strlen((char *)input);
@@ -51,32 +57,128 @@ static std::unique_ptr<uint8_t[]> padPkcs5(uint8_t *input, size_t inputLength,
   return std::move(output);
 }
 
-std::unique_ptr<uint8_t[]> encryptAes(uint8_t *input, uint8_t *key, uint8_t *iv)
+static std::unique_ptr<uint8_t[]> depadPkcs5(uint8_t *input, size_t inputLength)
+{
+  std::unique_ptr<uint8_t[]> result(new uint8_t[inputLength]);
+
+  const auto padBytes = input[inputLength - 1];
+
+  memcpy(result.get(), input, inputLength);
+
+  if (padBytes < 16)
+  {
+    for (auto i = inputLength - 1; i >= inputLength - padBytes; --i)
+    {
+      if (result.get()[i] != padBytes)
+      {
+        break;
+      }
+
+      result.get()[i] = 0;
+    }
+  }
+
+  return std::move(result);
+}
+
+std::unique_ptr<uint8_t[]> encryptAes(uint8_t *input, uint8_t *key, uint8_t *iv,
+                                      uint16_t &outputSize)
 {
   // TODO verify key length
 
-  const auto inputLength  = strlen((char *)input);
-  const auto outputLength = inputLength % 16 == 0
-                                ? inputLength
-                                : inputLength + (16 - inputLength % 16);
+  const auto inputLength = strlen((char *)input);
+  ESP_LOGI(TAG, "Plaintext size: %d", inputLength);
+  ESP_LOG_BUFFER_HEX(TAG, key, 32);
+  ESP_LOG_BUFFER_HEX(TAG, iv, 16);
 
-  std::unique_ptr<uint8_t[]> output(new uint8_t[outputLength]);
+  outputSize = inputLength % 16 == 0 ? inputLength
+                                     : inputLength + (16 - inputLength % 16);
 
-  mbedtls_aes_context context;
-  mbedtls_aes_setkey_enc(&context, key, 256);
+  std::unique_ptr<uint8_t[]> output(new uint8_t[outputSize]);
 
-  if (outputLength != inputLength)
+  esp_aes_context context;
+  esp_aes_init(&context);
+  esp_aes_setkey(&context, key, 256);
+
+  int returnValue = 0;
+
+  if (outputSize != inputLength)
   {
-    mbedtls_aes_crypt_cbc(&context, MBEDTLS_AES_ENCRYPT, outputLength, iv,
-                          padPkcs5(input, inputLength, outputLength).get(),
-                          output.get());
+    ESP_LOGI(TAG, "Input will be padded PKCS5");
+    auto paddedInput = padPkcs5(input, inputLength, outputSize);
+    ESP_LOG_BUFFER_HEX(TAG, paddedInput.get(), outputSize);
+
+    returnValue = esp_aes_crypt_cbc(&context, ESP_AES_ENCRYPT, outputSize, iv,
+                                    paddedInput.get(), output.get());
   }
   else
   {
-    mbedtls_aes_crypt_cbc(&context, MBEDTLS_AES_ENCRYPT, outputLength, iv,
-                          input, output.get());
+    returnValue = esp_aes_crypt_cbc(&context, ESP_AES_ENCRYPT, outputSize, iv,
+                                    input, output.get());
   }
 
+  esp_aes_free(&context);
+
+  ESP_LOGI(TAG, "Encrypt return value: %d", returnValue);
+  ESP_LOG_BUFFER_HEX(TAG, output.get(), outputSize);
+
   return std::move(output);
+}
+
+std::unique_ptr<uint8_t[]> decryptAes(uint8_t *input, uint16_t inputLength,
+                                      uint8_t *key, uint8_t *iv)
+{
+  ESP_LOGI(TAG, "Size of ciphertext: %d", inputLength);
+  ESP_LOG_BUFFER_HEX(TAG, key, 32);
+  ESP_LOG_BUFFER_HEX(TAG, iv, 16);
+
+  std::unique_ptr<uint8_t[]> output(new uint8_t[inputLength]);
+
+  esp_aes_context context;
+  esp_aes_init(&context);
+  esp_aes_setkey(&context, key, 256);
+
+  const auto returnValue = esp_aes_crypt_cbc(
+      &context, ESP_AES_DECRYPT, inputLength, iv, input, output.get());
+
+  ESP_LOGI(TAG, "Decrypt return value: %d", returnValue);
+  ESP_LOG_BUFFER_HEX(TAG, output.get(), inputLength);
+
+  auto depadded = depadPkcs5(output.get(), inputLength);
+
+  ESP_LOG_BUFFER_HEX(TAG, depadded.get(), inputLength);
+
+  esp_aes_free(&context);
+
+  return std::move(depadded);
+}
+
+std::unique_ptr<uint8_t[]> generateEcdhParams()
+{
+  std::unique_ptr<uint8_t[]> result(new uint8_t[32]);
+  mbedtls_ecdh_context       context;
+  mbedtls_ctr_drbg_context   ctrDrbg;
+  mbedtls_entropy_context    entropy;
+  const auto                *custom = "l33t";
+
+  mbedtls_ecdh_init(&context);
+  mbedtls_entropy_init(&entropy);
+
+  auto returnValue =
+      mbedtls_ecp_group_load(&context.grp, MBEDTLS_ECP_DP_BP256R1);
+  ESP_LOGI(TAG, "mbedtls_ecp_group_load return code: %d", returnValue);
+
+  returnValue = mbedtls_ctr_drbg_seed(&ctrDrbg, mbedtls_entropy_func, &entropy,
+                                      (uint8_t *)custom, strlen(custom));
+  ESP_LOGI(TAG, "mbedtls_ctr_drbg_seed return code: %d", returnValue);
+
+  returnValue = mbedtls_ecdh_gen_public(&context.grp, &context.d, &context.Q,
+                                        mbedtls_ctr_drbg_random, &ctrDrbg);
+  ESP_LOGI(TAG, "mbedtls_ecdh_gen_public return code: %d", returnValue);
+
+  returnValue = mbedtls_mpi_write_binary(&context.Q.X, result.get(), 32);
+  ESP_LOGI(TAG, "mbedtls_mpi_write_binary return code: %d", returnValue);
+
+  return std::move(result);
 }
 } // end namespace crypto
--- esp/lib/crypto-utils/crypto_utils.h
+++ esp/lib/crypto-utils/crypto_utils.h
@@ -9,7 +9,13 @@ std::unique_ptr<uint8_t[]> encodeBase64(uint8_t *input);
 
 std::unique_ptr<uint8_t[]> decodeBase64(uint8_t *input);
 
-std::unique_ptr<uint8_t[]> encryptAes(uint8_t *input);
+std::unique_ptr<uint8_t[]> encryptAes(uint8_t *input, uint8_t *key, uint8_t *iv,
+                                      uint16_t &outputSize);
+
+std::unique_ptr<uint8_t[]> decryptAes(uint8_t *input, uint16_t inputLength,
+                                      uint8_t *key, uint8_t *iv);
+
+std::unique_ptr<uint8_t[]> generateEcdhParams();
 } // end namespace crypto
 
 #endif // _CRYPTO_UTILS_H
