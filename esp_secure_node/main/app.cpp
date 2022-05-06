3bfbad306502760659fea6ad5b0435ec7ec2fc18
Andrei Cazacu <andrei.cazacu@oracle.com>
2022-05-06 14:33:25 +0300

    cleanup

--- esp_secure_node/main/app.cpp
+++ esp_secure_node/main/app.cpp
@@ -4,6 +4,12 @@
 #include "esp_log.h"
 #include "net_utils.h"
 
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/system_setup.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+
 // CONSTANTS
 static constexpr auto *TAG = "MAIN";
 static constexpr auto *CONTRACT_ADDRESS =
