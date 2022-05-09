519d8bccebd95a0193c4a762a0b2d20292ee15af
Andrei Cazacu <andrei.cazacu@oracle.com>
2022-05-09 15:53:43 +0300

    ml client

--- esp_secure_node/components/ml-utils/ml_utils.cpp
+++ esp_secure_node/components/ml-utils/ml_utils.cpp
@@ -1,12 +1,40 @@
 #include "ml_utils.h"
 
-#include "tensorflow/lite/micro/all_ops_resolver.h"
-#include "tensorflow/lite/micro/micro_error_reporter.h"
-#include "tensorflow/lite/micro/micro_interpreter.h"
-#include "tensorflow/lite/micro/system_setup.h"
-#include "tensorflow/lite/schema/schema_generated.h"
+#include "esp_log.h"
 
-namespace ml
+#define TENSOR_ARENA_SIZE 2048
+
+static tflite::MicroErrorReporter errorReporter;
+static tflite::AllOpsResolver     resolver;
+static constexpr auto            *TAG = "ML";
+static uint8_t                    tensorArena[TENSOR_ARENA_SIZE];
+
+MlPredictor::MlPredictor(const uint8_t *modelBytes)
+    : interpreter(tflite::GetModel(modelBytes), resolver, tensorArena,
+                  TENSOR_ARENA_SIZE, &errorReporter)
 {
-void test() {}
-} // end namespace ml
+  if (interpreter.AllocateTensors() != kTfLiteOk)
+  {
+    ESP_LOGE(TAG, "Tensors could not be allocated");
+  }
+
+  const auto arenaUsedBytes = interpreter.arena_used_bytes();
+  ESP_LOGI(TAG, "Arena used size: %d", arenaUsedBytes);
+}
+
+float *MlPredictor::getInputBuffer()
+{
+  return interpreter.input(0)->data.f;
+}
+
+float *MlPredictor::predict()
+{
+  ESP_LOGI(TAG, "Calling ML predict");
+
+  if (interpreter.Invoke() != kTfLiteOk)
+  {
+    ESP_LOGE(TAG, "Invocation failed");
+  }
+
+  return interpreter.output(0)->data.f;
+}
--- esp_secure_node/components/ml-utils/ml_utils.h
+++ esp_secure_node/components/ml-utils/ml_utils.h
@@ -1,9 +1,23 @@
 #ifndef _ML_UTILS_H
 #define _ML_UTILS_H
 
-namespace ml
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/system_setup.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+
+class MlPredictor
 {
-void test();
-} // end namespace ml
+public:
+  explicit MlPredictor(const uint8_t *modelBytes);
+
+  float *predict();
+
+  float *getInputBuffer();
+
+private:
+  tflite::MicroInterpreter interpreter;
+};
 
 #endif // _ML_UTILS_H
