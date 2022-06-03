0d7bf93dc17a45a7d11c661a26eed235932a88c1
Andrei Cazacu <andrei.cazacu@oracle.com>
2022-06-03 22:54:59 +0300

    edited script

--- cloudapps/dbapp/start.sh
+++ cloudapps/dbapp/start.sh
@@ -3,4 +3,4 @@
 echo 'starting container...'
 $JAVA_HOME/bin/keytool -importcert -trustcacerts -cacerts -storepass changeit -noprompt -file /usr/app/ca.crt -alias ownCA
 
-java -jar "/usr/app/dbapp-0.0.1-SNAPSHOT.jar"
+java -Dvault.token=$WRAPPING_TOKEN -jar "/usr/app/dbapp-0.0.1-SNAPSHOT.jar"
