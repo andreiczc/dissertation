2d751c6a74d444fc0b15b956868127ad41c7101e
Andrei Cazacu <andrei.cazacu@oracle.com>
2022-06-03 23:21:30 +0300

    integrated with docker compose

--- cloudapps/package.sh
+++ cloudapps/package.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+mvn -f config-server/pom.xml package -DskipTests
 mvn -f registry/pom.xml package -DskipTests
 mvn -f gateway/pom.xml package -DskipTests
 mvn -f dbapp/pom.xml package -DskipTests
+docker build -t config-server ./config-server
 docker build -t registry ./registry
 docker build -t gateway ./gateway
 docker build -t dbapp ./dbapp
