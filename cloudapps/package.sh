984f2f4c38cee5ef6497a21e0e3458ff2702f614
Andrei Cazacu <andrei.cazacu@oracle.com>
2022-06-23 16:25:57 +0300

    modified package script

--- cloudapps/package.sh
+++ cloudapps/package.sh
@@ -3,7 +3,9 @@ mvn -f config-server/pom.xml package -DskipTests
 mvn -f registry/pom.xml package -DskipTests
 mvn -f gateway/pom.xml package -DskipTests
 mvn -f dbapp/pom.xml package -DskipTests
+mvn -f mqtt-client/pom.xml package -DskipTests
 docker build -t config-server ./config-server
 docker build -t registry ./registry
 docker build -t gateway ./gateway
 docker build -t dbapp ./dbapp
+docker mqtt-client -t dbapp ./dbapp
