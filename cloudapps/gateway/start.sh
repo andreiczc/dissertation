74d36d08f8bfd9c3973dd73c317d7b374de33b70
Andrei Cazacu <andrei.cazacu@oracle.com>
2022-06-29 10:33:05 +0300

    edited dockerfile

--- cloudapps/gateway/start.sh
+++ cloudapps/gateway/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo 'starting dbapp...'
+echo 'starting gateway...'
 $JAVA_HOME/bin/keytool -importcert -trustcacerts -cacerts -storepass changeit -noprompt -file /usr/app/ca.crt -alias ownCA
 
 /usr/app/wait.sh registry:8761 --timeout=0 --strict -- java -jar /usr/app/gateway-0.0.1-SNAPSHOT.jar
