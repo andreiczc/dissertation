56c984ad745c1ce0f5c6f828dde65e1d8c4dd17d
Andrei Cazacu <andrei.cazacu@oracle.com>
2022-07-03 11:26:07 +0300

    changed certificates

--- cloudapps/package.sh
+++ cloudapps/package.sh
@@ -3,9 +3,7 @@ mvn -f config-server/pom.xml package -DskipTests
 mvn -f registry/pom.xml package -DskipTests
 mvn -f gateway/pom.xml package -DskipTests
 mvn -f dbapp/pom.xml package -DskipTests
-mvn -f mqtt-client/pom.xml package -DskipTests
 docker build -t config-server ./config-server
 docker build -t registry ./registry
 docker build -t gateway ./gateway
 docker build -t dbapp ./dbapp
-docker build -t mqtt-client ./mqtt-client
